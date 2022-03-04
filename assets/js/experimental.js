53c2d0378fa94e8acbbb346382b029db45b3f481
Matthew Pryor <matthewapryor@gmail.com>
2022-03-04 16:11:36 -0500

    create game-modes section, needs styling

--- assets/js/experimental.js
+++ assets/js/experimental.js
@@ -1,65 +1,65 @@
 
-// "COMPLETE CHECKPOINT" - Player enters radius of checkpoint.
-// "FINISH" - if { Player entered checkpoints the Course in consecutive order. } else { // print(currentCheckpoint)}
-// "START CIRCUIT" - If player is within proximity of the Course. Secondary implementation: Start only if course is unlocked to player.
+// // "COMPLETE CHECKPOINT" - Player enters radius of checkpoint.
+// // "FINISH" - if { Player entered checkpoints the Course in consecutive order. } else { // print(currentCheckpoint)}
+// // "START CIRCUIT" - If player is within proximity of the Course. Secondary implementation: Start only if course is unlocked to player.
 
-function placeCheckpoint() {
-    // getTapLocation();
-    // ifCheckpointAtTapLocation();
-    // addCheckpointToCurrentCourse();
-    // addCheckpointToMap();
+// function placeCheckpoint() {
+//     // getTapLocation();
+//     // ifCheckpointAtTapLocation();
+//     // addCheckpointToCurrentCourse();
+//     // addCheckpointToMap();
 
-    if(checkpoints >= minCheckpoints && checkpoints <= maxCheckpoints) { 
+//     if(checkpoints >= minCheckpoints && checkpoints <= maxCheckpoints) { 
         
-    } else if(checkpoints <= 1) { 
-        print("You must place at least two checkpoints to create a course.")
-    } else if(checkpoints == 49) { 
-        print("Checkpoint limit reached.")
-    } else { 
-        setIdToCheckpointOrder();
+//     } else if(checkpoints <= 1) { 
+//         print("You must place at least two checkpoints to create a course.")
+//     } else if(checkpoints == 49) { 
+//         print("Checkpoint limit reached.")
+//     } else { 
+//         setIdToCheckpointOrder();
 
    
-}
+// }
 
-class Checkpoint {
-    // var id :
-    // let checkpointOrder =
-    // let location =
-    // var isStart = false;
-    // var isFinish = false;
+// class Checkpoint {
+//     // var id :
+//     // let checkpointOrder =
+//     // let location =
+//     // var isStart = false;
+//     // var isFinish = false;
 
 
-}
+// }
 
-function setIdToCheckpointOrder() {
-    var checkpoints = [Checkpoint]
-}
+// function setIdToCheckpointOrder() {
+//     var checkpoints = [Checkpoint]
+// }
 
-function createCourse() {
-    let checkpoint = Checkpoint
-    let checkpoints = [Checkpoint]
-    let checkpointCount = Int
-    var isSaved = false;
+// function createCourse() {
+//     let checkpoint = Checkpoint
+//     let checkpoints = [Checkpoint]
+//     let checkpointCount = Int
+//     var isSaved = false;
    
-    // LOOP THROUGH ALL PLACED CHECKPOINTS
-    for(var i = 0; i < checkpointArray.length-1; i++) {
+//     // LOOP THROUGH ALL PLACED CHECKPOINTS
+//     for(var i = 0; i < checkpointArray.length-1; i++) {
 
-        return 
-    }
-}
+//         return 
+//     }
+// }
 
-function saveCourse() {
-}
+// function saveCourse() {
+// }
 
-function saveCourseToDrafts() {
-    // print("Your new course was saved. To edit this course you can find it in your 'Drafts'")
+// function saveCourseToDrafts() {
+//     // print("Your new course was saved. To edit this course you can find it in your 'Drafts'")
    
-}
+// }
 
 
 
-function loadCourses() {
-    var user = new User()
-    var myCourses = [Course]
+// function loadCourses() {
+//     var user = new User()
+//     var myCourses = [Course]
     
-}
+// }
