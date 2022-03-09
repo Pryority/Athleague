5c89100d85c426c8bc2135e5c91daf8b4216518f
Matthew Pryor <matthewapryor@gmail.com>
2022-03-09 10:28:43 -0500

    trying to structure logic of course creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,69 @@
+let name = ""
+let type = ["START", "END", "DEFAULT"]
+let location = {
+  easting: Number,
+  northing: Number,
+};
 
-var cpArray = [];
-console.log(cpArray)
+var courseInfo = {
+  name: window.prompt("What is your Course's name?"),
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function() {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  }
+};
 
-var addCheckpoint = function addCheckpoint() {
-   cpArray = cpArray + 1;
-   console.log(cpArray.length);
-   if (cpArray.length === 49) {
-       console.log("Maximum amount of checkpoints reached.")
-   }
+var checkpoint = {
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function() {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  }
+};
+
+var addCheckpoints = function() {
+  
 }
 
-var addCheckpoints = function addCheckpoint() {
-    cpArray = cpArray + 1;
-    console.log(cpArray.length);
-    if (cpArray.length === 49) {
-        console.log("Maximum amount of checkpoints reached.")
-    }
- }
-
-for (var i = 0; i < 48; i++) {
-    addCheckpoints();
+var load = function() {
+  for (var i = 0; i < 5; i++) {
+    
+  }
 }
+
+
+
+
+// class Checkpoint {
+//   constructor(name, type, loc, cpNum, cpLocations, avgDis) {
+//     this.name = "WAHOO!"
+//     this.type = type[2]
+//   }
+
+//   create(name, location) {
+
+//   }
+// }
+
+class Course {
+  constructor(name, type, loc, cpNum, cpLocations, avgDis) {
+    this.name = "WAHOO!"
+    this.type = type[2]
+  }
+
+  create(name, location) {
+
+  }
+}
+
+
+var course = new Course()
+console.log(course.name)
+console.log(course.type)
